7d21c837100891b5729fe093aaa5fc4d3506039c
katakanan <>
2025-02-18 12:37:32 +0900

    vectorize

--- ch04/main.py
+++ ch04/main.py
@@ -1,14 +1,20 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 from tensorflow.keras.datasets import imdb
+import numpy as np
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        for j in sequence:
+            results[i, j] = 1.
+
+    return results
 
 if __name__ == "__main__":
     (train_data, train_label), (test_data, test_label) = imdb.load_data(num_words = 10000)
 
-    # print(train_data[0])
-    # print(train_label[0])
-
-    word_index = imdb.get_word_index()
-    reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-    decoded_review = " ".join([reverse_word_index.get(i - 3, "?") for i in train_data[0]])
-    print(decoded_review)
+    x_train = vectorize_sequences(train_data)
+    x_test = vectorize_sequences(test_data)
+    y_train  = np.asarray(train_label).astype("float32")
+    y_test = np.asarray(test_label).astype("float32")
