aa980230e303084c5c2ae24e069372feed5b8641
katakanan <>
2025-02-18 12:51:16 +0900

    epochs = 4 is good

--- ch04/main.py
+++ ch04/main.py
@@ -29,32 +29,36 @@ if __name__ == "__main__":
                             ])
     model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
 
-    x_val = x_train[:10000]
-    partial_x_train = x_train[10000:]
-    y_val = y_train[:10000]
-    partial_y_train = y_train[10000:]
+    # x_val = x_train[:10000]
+    # partial_x_train = x_train[10000:]
+    # y_val = y_train[:10000]
+    # partial_y_train = y_train[10000:]
 
-    history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
-    history_dict = history.history
-    print(history_dict.keys())
-    loss_values = history_dict["loss"]
-    val_loss_values = history_dict["val_loss"]
-    epochs = range(1, len(loss_values) + 1)
-    plt.figure()
-    plt.plot(epochs, loss_values, "bo", label="Training loss")
-    plt.plot(epochs, val_loss_values, "b", label="Validation loss")
-    plt.title("Training and Validation loss")
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.legend()
+    # history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+    model.fit(x_train, y_train, epochs=4, batch_size=512, validation_data=(x_test, y_test))
+    results = model.evaluate(x_test, y_test)
+    print(results)
 
-    plt.figure()
-    acc = history_dict["accuracy"]
-    val_acc = history_dict["val_accuracy"]
-    plt.plot(epochs, acc, "bo", label="Training acc")
-    plt.plot(epochs, val_acc, "b", label="Validation acc")
-    plt.title("Training and Validation acc")
-    plt.xlabel("Epochs")
-    plt.ylabel("Accucary")
-    plt.legend()
-    plt.show()
+    # history_dict = history.history
+    # print(history_dict.keys())
+    # loss_values = history_dict["loss"]
+    # val_loss_values = history_dict["val_loss"]
+    # epochs = range(1, len(loss_values) + 1)
+    # plt.figure()
+    # plt.plot(epochs, loss_values, "bo", label="Training loss")
+    # plt.plot(epochs, val_loss_values, "b", label="Validation loss")
+    # plt.title("Training and Validation loss")
+    # plt.xlabel("Epochs")
+    # plt.ylabel("Loss")
+    # plt.legend()
+
+    # plt.figure()
+    # acc = history_dict["accuracy"]
+    # val_acc = history_dict["val_accuracy"]
+    # plt.plot(epochs, acc, "bo", label="Training acc")
+    # plt.plot(epochs, val_acc, "b", label="Validation acc")
+    # plt.title("Training and Validation acc")
+    # plt.xlabel("Epochs")
+    # plt.ylabel("Accucary")
+    # plt.legend()
+    # plt.show()
