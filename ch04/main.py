b052f8f8fea216e8fbee969faec1137d217172c3
katakanan <>
2025-02-18 12:48:43 +0900

    Training and Validation acc

--- ch04/main.py
+++ ch04/main.py
@@ -40,10 +40,21 @@ if __name__ == "__main__":
     loss_values = history_dict["loss"]
     val_loss_values = history_dict["val_loss"]
     epochs = range(1, len(loss_values) + 1)
+    plt.figure()
     plt.plot(epochs, loss_values, "bo", label="Training loss")
     plt.plot(epochs, val_loss_values, "b", label="Validation loss")
     plt.title("Training and Validation loss")
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
+    plt.legend()
+
+    plt.figure()
+    acc = history_dict["accuracy"]
+    val_acc = history_dict["val_accuracy"]
+    plt.plot(epochs, acc, "bo", label="Training acc")
+    plt.plot(epochs, val_acc, "b", label="Validation acc")
+    plt.title("Training and Validation acc")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accucary")
     plt.legend()
     plt.show()
