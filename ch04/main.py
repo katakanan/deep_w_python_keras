445efd3e6df45760f3b3062f432f59542f0acef5
katakanan <>
2025-02-18 12:46:28 +0900

    Training loss and Validation loss

--- ch04/main.py
+++ ch04/main.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
     loss_values = history_dict["loss"]
     val_loss_values = history_dict["val_loss"]
     epochs = range(1, len(loss_values) + 1)
+    plt.plot(epochs, loss_values, "bo", label="Training loss")
     plt.plot(epochs, val_loss_values, "b", label="Validation loss")
     plt.title("Training and Validation loss")
     plt.xlabel("Epochs")
