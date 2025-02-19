55b366b3db049259b1daacaf3d18b01f161e7a6c
katakanan <>
2025-02-19 09:25:47 +0900

    boston housing k fold cross val

---
+++ ch04/main3.py
@@ -0,0 +1,47 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import boston_housing
+import numpy as np
+import matplotlib.pyplot as plt
+
+def build_model():
+    model = keras.Sequential([
+                                layers.Dense(64, activation="relu"),
+                                layers.Dense(64, activation="relu"),
+                                layers.Dense(1),
+                            ])
+    model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
+    return model
+
+if __name__ == "__main__":
+    (train_data, train_targets), (test_data, test_targets) = (boston_housing.load_data())
+    # print(train_data.shape)
+    # print(test_data.shape)
+    # print(train_targets)
+
+    mean = train_data.mean(axis=0)
+    train_data -= mean
+    std = train_data.std(axis=0)
+    train_data /= std
+    test_data -= mean
+    test_data /= std
+
+    k = 4
+    num_val_samples = len(train_data) // k
+    num_epochs = 100
+    all_scores = []
+    for i in range(k):
+        print(f"Processing fold #{i}")
+        val_data = train_data[i*num_val_samples:(i+1)*num_val_samples]
+        val_targets = train_targets[i*num_val_samples:(i+1)*num_val_samples]
+        partial_train_data = np.concatenate([train_data[:i*num_val_samples], train_data[(i+1)*num_val_samples:]], axis=0)
+        partial_train_targets = np.concatenate([train_targets[:i*num_val_samples], train_targets[(i+1)*num_val_samples:]], axis=0)
+        model = build_model()
+        model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=16, verbose=0)
+        val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+        all_scores.append(val_mae)
+
+    print(all_scores)
+    print(np.mean(all_scores))
