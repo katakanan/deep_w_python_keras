6e8b24037eb8f10dbc1f68e8c18d1cf6b98a99d1
katakanan <>
2025-02-19 09:44:47 +0900

    mae vs epochs

--- ch04/main3.py
+++ ch04/main3.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
 
     k = 4
     num_val_samples = len(train_data) // k
-    num_epochs = 100
-    all_scores = []
+    num_epochs = 500
+    all_mae_histories = []
     for i in range(k):
         print(f"Processing fold #{i}")
         val_data = train_data[i*num_val_samples:(i+1)*num_val_samples]
@@ -39,9 +39,15 @@ if __name__ == "__main__":
         partial_train_data = np.concatenate([train_data[:i*num_val_samples], train_data[(i+1)*num_val_samples:]], axis=0)
         partial_train_targets = np.concatenate([train_targets[:i*num_val_samples], train_targets[(i+1)*num_val_samples:]], axis=0)
         model = build_model()
-        model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=16, verbose=0)
-        val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
-        all_scores.append(val_mae)
+        history = model.fit(partial_train_data, partial_train_targets, 
+                            validation_data = (val_data, val_targets),
+                            epochs=num_epochs, batch_size=16, verbose=0)
+        mae_history = history.history["val_mae"]
+        all_mae_histories.append(mae_history)
 
-    print(all_scores)
-    print(np.mean(all_scores))
+    average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+    truncated_mae_history = average_mae_history[10:]
+    plt.plot(range(1, len(truncated_mae_history) + 1), truncated_mae_history)
+    plt.xlabel("Epochs")
+    plt.ylabel("Validation MAE")
+    plt.show()
