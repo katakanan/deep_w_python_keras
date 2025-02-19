ddd3513f4265414174e7d76a638552817407ee8f
katakanan <>
2025-02-19 09:49:57 +0900

    epoch is 130

--- ch04/main3.py
+++ ch04/main3.py
@@ -28,26 +28,30 @@ if __name__ == "__main__":
     test_data -= mean
     test_data /= std
 
-    k = 4
-    num_val_samples = len(train_data) // k
-    num_epochs = 500
-    all_mae_histories = []
-    for i in range(k):
-        print(f"Processing fold #{i}")
-        val_data = train_data[i*num_val_samples:(i+1)*num_val_samples]
-        val_targets = train_targets[i*num_val_samples:(i+1)*num_val_samples]
-        partial_train_data = np.concatenate([train_data[:i*num_val_samples], train_data[(i+1)*num_val_samples:]], axis=0)
-        partial_train_targets = np.concatenate([train_targets[:i*num_val_samples], train_targets[(i+1)*num_val_samples:]], axis=0)
-        model = build_model()
-        history = model.fit(partial_train_data, partial_train_targets, 
-                            validation_data = (val_data, val_targets),
-                            epochs=num_epochs, batch_size=16, verbose=0)
-        mae_history = history.history["val_mae"]
-        all_mae_histories.append(mae_history)
+    # k = 4
+    # num_val_samples = len(train_data) // k
+    # num_epochs = 500
+    # all_mae_histories = []
+    # for i in range(k):
+    #     print(f"Processing fold #{i}")
+    #     val_data = train_data[i*num_val_samples:(i+1)*num_val_samples]
+    #     val_targets = train_targets[i*num_val_samples:(i+1)*num_val_samples]
+    #     partial_train_data = np.concatenate([train_data[:i*num_val_samples], train_data[(i+1)*num_val_samples:]], axis=0)
+    #     partial_train_targets = np.concatenate([train_targets[:i*num_val_samples], train_targets[(i+1)*num_val_samples:]], axis=0)
+    #     model = build_model()
+    #     history = model.fit(partial_train_data, partial_train_targets, 
+    #                         validation_data = (val_data, val_targets),
+    #                         epochs=num_epochs, batch_size=16, verbose=0)
+    #     mae_history = history.history["val_mae"]
+    #     all_mae_histories.append(mae_history)
 
-    average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
-    truncated_mae_history = average_mae_history[10:]
-    plt.plot(range(1, len(truncated_mae_history) + 1), truncated_mae_history)
-    plt.xlabel("Epochs")
-    plt.ylabel("Validation MAE")
-    plt.show()
+    # average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+    # truncated_mae_history = average_mae_history[10:]
+    # plt.plot(range(1, len(truncated_mae_history) + 1), truncated_mae_history)
+    # plt.xlabel("Epochs")
+    # plt.ylabel("Validation MAE")
+    # plt.show()
+    model = build_model()
+    model.fit(train_data, train_targets, epochs=130, batch_size=16, verbose=0)
+    test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
+    print(test_mae_score)
